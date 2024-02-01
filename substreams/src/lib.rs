5812533e4d7088823e10f802143407137fb0c0db
arnaudberger <arnaudberger84@gmail.com>
2024-02-01 09:38:34 -0500

    removing unecessary import

--- substreams/src/lib.rs
+++ substreams/src/lib.rs
@@ -1,9 +1,6 @@
 use std::time::SystemTime;
-
 use substreams::pb::substreams::Clock;
 use substreams_sink_kv::pb::sf::substreams::sink::kv::v1::KvOperations;
-
-mod pb;xw
 const KEY_BLOCK_BY_ID_NUMBER_PREFIX: &str = "bi";
 const KEY_BLOCK_BY_NUMBER_ID_PREFIX: &str = "bn";
 const KEY_BLOCK_BY_TIMESTAMP_ID_PREFIX: &str = "bt";
@@ -21,10 +18,10 @@ fn map_clocks(clock: Clock) -> Result<KvOperations, substreams::errors::Error> {
     let key_timestamp_id = format!("{}:{}:{}", KEY_BLOCK_BY_TIMESTAMP_ID_PREFIX, unix_millis, clock.number);
 
     let unix_millis_bytes = unix_millis.to_be_bytes().to_vec();
+
     kv_ops.push_new(key_id_number, &unix_millis_bytes, 1);
     kv_ops.push_new(key_number_id, &unix_millis_bytes, 1);
     kv_ops.push_new(key_timestamp_id, clock.number.to_be_bytes(), 1);
-
 
     Ok(kv_ops)
 }
