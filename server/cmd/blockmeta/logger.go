643b51bf296e5dc4cc0eb55d1ee79971c353f855
colindickson <colin.dickson@gmail.com>
2024-02-08 08:40:50 -0500

    blockmeta: update cmd

---
+++ server/cmd/blockmeta/logger.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+)
+
+var logger *slog.Logger
+
+func init() {
+	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+}
--- server/cmd/blockmeta/main.go
+++ server/cmd/blockmeta/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net"
+	"os"
 
 	pbbmsrv "github.com/streamingfast/blockmeta-service/pb/sf/blockmeta/v2"
 	"github.com/streamingfast/blockmeta-service/service"
@@ -12,17 +12,23 @@ import (
 )
 
 var (
-	listenAddress     = flag.String("grpc-listen-addr ", ":9000", "The server port")
-	sinkServerAddress = flag.String("grpc-listen-addr ", "", "The server port")
+	listenAddress     = flag.String("grpc-listen-addr", ":9000", "The gRPC server listen address")
+	sinkServerAddress = flag.String("sink-addr", "", "The sink server address")
 )
 
 // todo: convert to cobra and viper
 func main() {
 	flag.Parse()
 
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *listenAddress))
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s", *listenAddress))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		logger.Error("failed to listen", "error", err, "address", *listenAddress)
+		os.Exit(1)
+	}
+
+	if *sinkServerAddress == "" {
+		logger.Error("sink server address is required")
+		os.Exit(1)
 	}
 
 	sinkClient := service.ConnectToSinkServer(*sinkServerAddress)
@@ -33,8 +39,9 @@ func main() {
 	pbbmsrv.RegisterBlockServer(sBlock, blockService)
 	pbbmsrv.RegisterBlockByTimeServer(sBlock, blockByTimeService)
 
-	log.Printf("server block listening at %v", listener.Addr())
+	logger.Info("server block listening", "address", listener.Addr())
 	if err := sBlock.Serve(listener); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		logger.Error("failed to serve", "error", err)
+		os.Exit(1)
 	}
 }
